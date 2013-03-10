e2a58a41ce36bd1bd7b1f955bda7888f8ab0a046
Luis Reis <luis.m.reis@gmail.com>
2013-03-10 22:42:55 +0000

    Send individual commands instead of batching.

--- driver.c
+++ driver.c
@@ -16,8 +16,9 @@
 
 #define LCD_SPI_BITS_PER_WORD 9
 #define LCD_SPI_MODE SPI_MODE_0
-#define LCD_SPI_SPEED 3815
-// #define LCD_SPI_SPEED 3000000
+// #define LCD_SPI_SPEED 3815 // Minimum
+// #define LCD_SPI_SPEED 3000000 // From the LCD datasheet
+#define LCD_SPI_SPEED 100000
 
 static int spi_init(char * dev) {
   int fd;
@@ -44,48 +45,39 @@ static int spi_init(char * dev) {
   return fd;
 }
 
-static void send_cmd() {
-  
-  return ioctl(lcd->fd, SPI_IOC_MESSAGE(ptx - tx), tx);
+static void send_cmd(LCD *lcd,
+                     uint8_t cmd,
+                     unsigned char *data,
+                     unsigned int data_len) {
+
+  uint32_t stride = 1;//2 * 1024; // Max = 2048
+  uint32_t len, i;
+  uint16_t buf[1 + data_len];
+  struct spi_ioc_transfer tx;
+
+  buf[0] = cmd;
+  for (i = 0; i < data_len; ++i)
+    buf[i + 1] = data[i] | 0x0100;
+  len = data_len + 1;
+
+  bzero(&tx, sizeof(struct spi_ioc_transfer));
+  tx.tx_buf = (uint32_t) buf;
+  tx.rx_buf = (uint32_t) 0;
+  tx.len = 2;//len;
+  tx.bits_per_word = LCD_SPI_BITS_PER_WORD;
+  tx.speed_hz = LCD_SPI_SPEED;
+  tx.delay_usecs = (uint16_t) 0;
+
+  for (i = 0; i < len; i += stride) {
+    tx.tx_buf = (uint32_t) &buf[i];
+    tx.len = 2 * (i + stride > len ? len - i : stride);
+
+    if (ioctl(lcd->fd, SPI_IOC_MESSAGE(1), &tx) < 0) {
+      perror("Error sending SPI message");
+      exit(1);
+    }
+  }
 }
-
-static void add_cmd(
-  struct spi_ioc_transfer **ptx,
-  uint16_t **pbuf,
-  char cmd,
-  unsigned char *data,
-  unsigned int data_len
-  ) {
-
-  struct spi_ioc_transfer *tx;
-  uint16_t *buf;
-  uint32_t len;
-  int i;
-
-  if (!data)
-    data_len = 0;
-
-  len = 2 * (1 + data_len);
-
-  tx = *ptx;
-  buf = *pbuf;
-
-  bzero(tx, sizeof(struct spi_ioc_transfer));
-  tx->tx_buf = (uint32_t) buf;
-  tx->rx_buf = (uint32_t) 0;
-  tx->len = len;
-  tx->bits_per_word = LCD_SPI_BITS_PER_WORD;
-  tx->speed_hz = LCD_SPI_SPEED;
-  tx->delay_usecs = (uint16_t) 0;
-
-  *ptx += 1;
-  *pbuf += data_len;
-
-  buf[0] = ((uint16_t)cmd & 0x00FF);
-  for (i=0; i<data_len; ++i)
-    buf[i+1] = ((uint16_t)(data[i]) & 0x00FF) | 0x0100;
-}
-
 
 int lcd_clear(LCD *lcd, int color) {
   uint8_t paset, caset, ramwr;
@@ -95,8 +87,6 @@ int lcd_clear(LCD *lcd, int color) {
   uint8_t xaset_data[] = { 0, 131 };
   uint8_t ramwr_data[ramwr_data_len];
 
-  struct spi_ioc_transfer tx[3], *ptx;
-  uint16_t buf[1+2 + 1+2 + 1+ramwr_data_len], *pbuf;
 
   uint32_t i;
 
@@ -106,9 +96,6 @@ int lcd_clear(LCD *lcd, int color) {
     ramwr_data[i+1] = ((color & 0x0F) << 4) | (color >> 8);
     ramwr_data[i+2] = color & 0x0FF;
   }
-
-  ptx = tx;
-  pbuf = buf;
 
   if (lcd->type == TYPE_EPSON) {
     paset = PASET;
@@ -120,19 +107,14 @@ int lcd_clear(LCD *lcd, int color) {
     ramwr = RAMWRP;
   }
 
-  add_cmd(&ptx, &pbuf, paset, xaset_data, 2);
-  add_cmd(&ptx, &pbuf, caset, xaset_data, 2);
-  add_cmd(&ptx, &pbuf, ramwr, ramwr_data, 0);
+  send_cmd(lcd, paset, xaset_data, 2);
+  send_cmd(lcd, caset, xaset_data, 2);
+  send_cmd(lcd, ramwr, ramwr_data, 0);
 
-  return ioctl(lcd->fd, SPI_IOC_MESSAGE(ptx - tx), tx);
+  return 0;
 }
 
 int lcd_init(LCD *lcd, char *dev, int type) {
-  struct spi_ioc_transfer tx[10], *ptx;
-  uint16_t buf[1+4 + 1+1 + 1 + 1 + 1+1 + 1 + 1+5 + 1+3 + 1 + 1], *pbuf;
-
-  ptx = tx;
-  pbuf = buf;
 
   bzero(lcd, sizeof(LCD));
   lcd->dev = dev;
@@ -144,54 +126,48 @@ int lcd_init(LCD *lcd, char *dev, int type) {
   // display control
   // 12 = 1100 - CL dividing ratio [don't divide] switching period 8H (default)
   unsigned char disctl_data[] = { 0x0c, 0x20, 0x00, 0x01 };
-  add_cmd(&ptx, &pbuf, DISCTL, disctl_data, sizeof(disctl_data));
+  send_cmd(lcd, DISCTL, disctl_data, sizeof(disctl_data));
 
   // common scanning direction
   unsigned char comscn_data[] = { 0x01 };
-  add_cmd(&ptx, &pbuf, COMSCN, comscn_data, sizeof(comscn_data));
+  send_cmd(lcd, COMSCN, comscn_data, sizeof(comscn_data));
 
-  // internal oscialltor ON
-  add_cmd(&ptx, &pbuf, OSCON, NULL, 0);
+  // internal oscillator ON
+  send_cmd(lcd, OSCON, NULL, 0);
 
   // sleep out
-  add_cmd(&ptx, &pbuf, SLPOUT, NULL, 0);
+  send_cmd(lcd, SLPOUT, NULL, 0);
 
   // power ctrl
   // everything on, no external reference resistors
   unsigned char pwrctr_data[] = { 0x0F };
-  add_cmd(&ptx, &pbuf, PWRCTR, pwrctr_data, sizeof(pwrctr_data));
+  send_cmd(lcd, PWRCTR, pwrctr_data, sizeof(pwrctr_data));
 
   // invert display mode
-  add_cmd(&ptx, &pbuf, DISINV, NULL, 0);
+  send_cmd(lcd, DISINV, NULL, 0);
 
   // data control
   // - 0x03 - correct for normal sin7
   // - 0x00 - normal RGB arrangement
   // - 0x02 - 16-bit Grayscale Type A
   unsigned char datctl_data[] = { 0x03, 0x00, 0x02, 0xc8, 0x02 };
-  add_cmd(&ptx, &pbuf, DATCTL, datctl_data, sizeof(datctl_data));
+  send_cmd(lcd, DATCTL, datctl_data, sizeof(datctl_data));
 
   // electronic volume, this is the contrast/brightness
   // - 0x24 - volume (contrast) setting - fine tuning, original
   // - 0x03 - internal resistor ratio - coarse adjustment
   unsigned char volctr_data[] = { 0x24, 0x03, 0x30 };
-  add_cmd(&ptx, &pbuf, VOLCTR, volctr_data, sizeof(volctr_data));
+  send_cmd(lcd, VOLCTR, volctr_data, sizeof(volctr_data));
 
   // nop
-  add_cmd(&ptx, &pbuf, NOP, NULL, 0);
-
-  if (ioctl(lcd->fd, SPI_IOC_MESSAGE(ptx - tx), tx) < 0)
-    return -1;
+  send_cmd(lcd, NOP, NULL, 0);
 
   usleep(200 * 1000);
 
-  ptx = tx;
-  pbuf = buf;
-
   // display on
-  add_cmd(&ptx, &pbuf, DISON, NULL, 0);
+  send_cmd(lcd, DISON, NULL, 0);
 
-  return ioctl(lcd->fd, SPI_IOC_MESSAGE(ptx - tx), tx);
+  return 0;
 }
 
 void lcd_dispose(LCD *lcd) {
