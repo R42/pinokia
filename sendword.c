6b931807bd010bd95ffbc35aa2581d4336206dbe
Luis Reis <luis.m.reis@gmail.com>
2013-03-30 17:27:24 +0000

    Test 9bit words, on uint16_t arrays, no rx buffer, no ioctl reads. (working\!)

--- sendword.c
+++ sendword.c
@@ -12,6 +12,7 @@
 
 #define SPI_MSB_FIRST       0
 #define SPI_BITS_PER_WORD_8 8
+#define SPI_BITS_PER_WORD_9 9
 #define SPI_SPEED_500KHZ    500 * 1000 // Raspberry Pi minimum
 #define SPI_SPEED_1MHZ      1000 * 1000
 #define SPI_SPEED_3815HZ    3815 // Linux minimum
@@ -23,16 +24,16 @@
 
 const uint32_t DELAY     = SPI_DELAY_0;
 const uint32_t SPEED     = SPI_SPEED_3815HZ;
-const uint32_t BPW       = SPI_BITS_PER_WORD_8;
+const uint32_t BPW       = SPI_BITS_PER_WORD_9;
 const uint32_t CS_CHANGE = SPI_CS_CHANGE;
 
 const char* SPI_DEV = "/dev/spidev0.0";
 
 // 25 == EPSON NOP
-uint8_t tx[] = {
-  0x00, 0x25,
+uint16_t tx[] = {
+  0x155, 0x0000, 0x0125, 0x0125,
 };
-uint8_t rx[ARRAY_SIZE(tx)] = {0, };
+uint16_t rx[ARRAY_SIZE(tx)] = {0, };
 
 static struct spi_ioc_transfer transfer;
 
@@ -48,13 +49,13 @@ static int spi_init(const char* dev) {
     return fd;
 
   if (ioctl(fd, SPI_IOC_WR_MODE         , &mode     ) < 0) return -1;
-  if (ioctl(fd, SPI_IOC_RD_MODE         , &mode     ) < 0) return -1;
+  // if (ioctl(fd, SPI_IOC_RD_MODE         , &mode     ) < 0) return -1;
   if (ioctl(fd, SPI_IOC_WR_LSB_FIRST    , &lsb_first) < 0) return -1;
-  if (ioctl(fd, SPI_IOC_RD_LSB_FIRST    , &lsb_first) < 0) return -1;
+  // if (ioctl(fd, SPI_IOC_RD_LSB_FIRST    , &lsb_first) < 0) return -1;
   if (ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bpw      ) < 0) return -1;
-  if (ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bpw      ) < 0) return -1;
+  // if (ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bpw      ) < 0) return -1;
   if (ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ , &speed    ) < 0) return -1;
-  if (ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ , &speed    ) < 0) return -1;
+  // if (ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ , &speed    ) < 0) return -1;
 
   printf("spi mode: %d\n"             , mode);
   printf("*lsb first: %d\n"           , lsb_first);
@@ -69,11 +70,11 @@ static uint32_t send_word(int fd, uint32_t word) {
 
   struct spi_ioc_transfer tr = {
     .tx_buf = (unsigned long) tx,
-    .rx_buf = (unsigned long) rx,
-    .len = ARRAY_SIZE(tx),
+    .rx_buf = (unsigned long) 0,//rx,
+    .len = 4, //ARRAY_SIZE(tx),
     .delay_usecs = DELAY,
     .speed_hz = SPEED,
-    .bits_per_word = 8,
+    .bits_per_word = BPW,
     .cs_change = 1,
   };
  
