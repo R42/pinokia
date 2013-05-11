48de9ae67b0091d85a9199a50da0c399a0d6d355
Luis Reis <luis.m.reis@gmail.com>
2013-05-11 10:36:54 +0100

    Fix Philips LCD support. (Changes missing from last commit).

--- main.c
+++ main.c
@@ -5,38 +5,51 @@
 #include <stdbool.h>
 #include <errno.h>
 
-#include "modules.h"
 #include "driver.h"
+
+#include <math.h>
+
+#define fracf(x) ((x) - floorf(x))
 
 int main(int argc, char * argv[]) {
   int res;
   LCD *lcd = (LCD *) malloc(sizeof(LCD));
 
-  // make sure SPI kernel modules are loaded
-  // loadSpiModules();
-
-  printf("Init'ing\n");
-  res = lcd_init(lcd, "/dev/spidev0.0", 25, TYPE_EPSON);
+  res = lcd_init(lcd, "/dev/spidev0.0", 25, TYPE_PHILIPS);
   if (res < 0) {
     perror("Failed to init LCD.");
     return 1;
   }
 
-  printf("Ready. Press [return] to clear the screen.\n");
-  getc(stdin);
+  lcd_clear(lcd, BLACK);
 
-  printf("Setting the screen green\n");
-  // res = lcd_set_pixel(lcd, 10, 10, GREEN);
-  // if (res < 0) {
-  //   perror("Failed to clear LCD.");
-  //   return 1;
+  // const int w = 130, h = 130;
+  // uint32_t x, y, color;
+  // float r, g, b, s2 = sqrtf(2), dd = w * s2;
+  // for (x = 0; x < w; x++) {
+  //   for (y = 0; y < h; y++) {
+  //     r = (dd - sqrtf(x * x + y * y            )) * 0xf / dd; if (r < 0) r = 0;
+  //     g = (dd - sqrtf((w - x) * (w - x) + y * y)) * 0xf / dd; if (g < 0) g = 0;
+  //     b = (dd - sqrtf(x * x + (h - y) * (h - y))) * 0xf / dd; if (b < 0) b = 0;
+  // 
+  //     color = RGB((int) r + (int) (1.0f * rand() / RAND_MAX + fracf(r)),
+  //                 (int) g + (int) (1.0f * rand() / RAND_MAX + fracf(g)),
+  //                 (int) b + (int) (1.0f * rand() / RAND_MAX + fracf(b)));
+  // 
+  //     lcd_set_pixel(lcd, x + 1, y + 1, color);
+  //   }
   // }
 
-  lcd_clear(lcd, GREEN);
-
-  printf("Ready. Press [return] to exit.\n");
-  getc(stdin);
-  // sleep(9);
+  int i;
+  int offset;
+  for (offset = 0; offset < 130 / 2; offset++) {
+    for (i = 130 - (2 * offset); i > 0; i--) {
+      lcd_set_pixel(lcd,   i + offset,   1 + offset, RED  );
+      lcd_set_pixel(lcd, 130 - offset,   i + offset, GREEN);
+      lcd_set_pixel(lcd,   i + offset, 130 - offset, BLUE );
+      lcd_set_pixel(lcd,   1 + offset,   i + offset, WHITE);
+    }
+  }
 
   lcd_dispose(lcd);
   free(lcd);
